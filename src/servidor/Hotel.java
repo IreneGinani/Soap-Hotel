2d12c99bb56c7d885dc27a53d6e30d8fa3f68574
Irene <ireneginani@gmail.com>
2018-10-09 21:28:21 -0300

    criando construtor do hotel

--- src/servidor/Hotel.java
+++ src/servidor/Hotel.java
@@ -4,14 +4,22 @@ import java.util.ArrayList;
 
 public class Hotel {
 
-	private int quantidade_quartos;
-	private ArrayList<ArrayList<Quarto>> quartos = new ArrayList<ArrayList<Quarto>>();
+	private ArrayList<Quarto> quartos = new ArrayList<Quarto>();
 	
 	// os três pontos serve para dizer que o tamanho das variáveis é flexível, podem ser 1 ou 1 milhão de tipos e precos, sendo par a par, 1
 	// preco 1 tipo. 2 preco 2 tipo e assim sucessivamente
 
-	public Hotel() { 
-		super();
+	public Hotel(String [] tipos, float [] precos, int [] quantidade_por_tipo) { 
 		
+		if ((tipos.length == precos.length) && (precos.length == quantidade_por_tipo.length)) {
+			for (int i : quantidade_por_tipo) {
+				for (int j = 0; j < quantidade_por_tipo[i]; j++) {
+					Quarto quarto = new Quarto(tipos[i], quantidade_por_tipo[i]);
+					quartos.add(quarto);
+				}
+			}
+		} else {
+			throw new IllegalArgumentException();
+		}
 	}
 }
